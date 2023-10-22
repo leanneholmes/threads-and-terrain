05a2e48e843372f1721b49d3271fc7a8301ef74f
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-22 01:30:46 -0700

    Template of store layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +7,8 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'threads&terrain';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http
-      .get<Pagination<Product[]>>(
-        'https://localhost:5001/api/products?pageSize=50'
-      )
-      .subscribe({
-        next: (response) => (this.products = response.data),
-        error: (error) => console.log(error),
-        complete: () => {
-          console.log('request completed');
-        },
-      });
-  }
+  ngOnInit(): void {}
 }
