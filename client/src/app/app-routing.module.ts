f94619ad351fdc19b44c3763478296149bef796a
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-23 14:18:34 -0700

    Added cart functionality

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -19,9 +19,14 @@ const routes: Routes = [
     loadChildren: () => import('./shop/shop.module').then((m) => m.ShopModule),
   },
   {
-    path: 'basket',
+    path: 'cart',
     loadChildren: () =>
       import('./basket/basket.module').then((m) => m.BasketModule),
+  },
+  {
+    path: 'checkout',
+    loadChildren: () =>
+      import('./checkout/checkout.module').then((m) => m.CheckoutModule),
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
