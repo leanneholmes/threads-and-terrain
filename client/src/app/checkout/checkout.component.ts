a6bdb9aa4518c3b76a9a30adc5e3c25273d61e2e
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-25 17:39:08 -0700

    Address, delivery forms

--- client/src/app/checkout/checkout.component.ts
+++ client/src/app/checkout/checkout.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent {
+  constructor(private fb: FormBuilder) {}
 
+  checkoutForm = this.fb.group({
+    addressForm: this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      province: ['', Validators.required],
+      postalcode: ['', Validators.required],
+    }),
+    deliveryForm: this.fb.group({
+      deliveyrMethod: ['', Validators.required],
+    }),
+    paymentForm: this.fb.group({
+      nameOnCard: ['', Validators.required],
+    }),
+  });
 }
