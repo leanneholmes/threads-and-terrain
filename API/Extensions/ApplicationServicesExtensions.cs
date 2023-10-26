f0a0a5fc8b6f1ae6aeb94494a3754962223d0612
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-26 15:50:46 -0700

    Persistent shipping price, Stripe integration

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -25,6 +25,7 @@ namespace API.Extensions
             services.AddScoped<IOrderService, OrderService>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IPaymentService, PaymentService>();
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-checkout-review',
   templateUrl: './checkout-review.component.html',
-  styleUrls: ['./checkout-review.component.scss']
+  styleUrls: ['./checkout-review.component.scss'],
 })
 export class CheckoutReviewComponent {
-  constructor(public basketService: BasketService) {}
+  constructor(
+    public basketService: BasketService,
+    private toastr: ToastrService
+  ) {}
+
+  createPaymentIntent() {
+    this.basketService.createPaymentIntent().subscribe({
+      next: () => this.toastr.success('Payment intent created'),
+      error: (error) => this.toastr.error(error.message),
+    });
+  }
 }
