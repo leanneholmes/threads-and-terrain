dfc16dcf75074f53d981a390505c697e6f34ceda
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-27 22:14:44 -0700

    Prep files for hosting

--- API/Helpers/CachedAttribute.cs
+++ API/Helpers/CachedAttribute.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace API.Helpers
@@ -22,7 +25,29 @@ namespace API.Helpers
 
             var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
             var cachedResponse = await cachedService.GetCachedResponse(cacheKey);
+
+            if (!string.IsNullOrEmpty(cachedResponse))
+            {
+                var contentResult = new ContentResult
+                {
+                    Content = cachedResponse,
+                    ContentType = "application/json",
+                    StatusCode = 200
+                };
+
+                context.Result = contentResult;
+
+                return;
+            }
+
+            var executedContext = await next();
+
+            if (executedContext.Result is OkObjectResult okObjectResult)
+            {
+                await cachedService.CacheResponseAsync(cacheKey, okObjectResult.Value, TimeSpan.FromSeconds(_timeToLiveSeconds));
+            }
         }
+
 
         private string GenerateCacheKeyFromRequest(HttpRequest request)
         {
--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ using Infrastructure.Data;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,6 +28,11 @@ app.UseSwaggerdocumentation();
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "Content")), RequestPath = "/Content"
+});
 
 app.UseCors("CorsPolicy");
 
@@ -34,6 +40,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
