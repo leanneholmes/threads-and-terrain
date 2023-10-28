dfc3c0acb6183bd392e04868ce0415199d438966
Leanne Holmes <lholmes24@my.bcit.ca>
2023-10-27 21:05:27 -0700

    Updated about page

---
+++ API/Helpers/CachedAttribute.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API.Helpers
+{
+    public class CachedAttribute : Attribute, IAsyncActionFilter
+    {
+        private readonly int _timeToLiveSeconds;
+        public CachedAttribute(int timeToLiveSeconds)
+        {
+            _timeToLiveSeconds = timeToLiveSeconds;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var cachedService = context.HttpContext.RequestServices.GetRequiredService<IResponseCacheService>();
+
+            var cacheKey = GenerateCacheKeyFromRequest(context.HttpContext.Request);
+            var cachedResponse = await cachedService.GetCachedResponse(cacheKey);
+        }
+
+        private string GenerateCacheKeyFromRequest(HttpRequest request)
+        {
+            var keyBuilder = new StringBuilder(); 
+
+            keyBuilder.Append($"{request.Path}");
+
+            foreach (var (key, value) in request.Query.OrderBy(x => x.Key))
+            {
+                keyBuilder.Append($"|{key}-{value}");
+            }
+
+            return keyBuilder.ToString();
+        }
+    }
+}
